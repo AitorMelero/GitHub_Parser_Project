fd89f7beb0f23ad0e0f86def6009f9c8410309ce
AitorMelero <meleropiconaitor@gmail.com>
2022-04-08 18:00:31 +0200

    Add: Colocación principal de componentes en la pantalla del login

--- src/main/java/gpp/view/window/login/VLogin.java
+++ src/main/java/gpp/view/window/login/VLogin.java
@@ -1,10 +1,14 @@
 package gpp.view.window.login;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.Font;
 
 import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -23,8 +27,11 @@ import gpp.view.component.VColor;
 public class VLogin extends JPanel {
 
 	private JPanel container; // Contenedor general
+	private JPanel containerTitle;  // Contenedor para el título
 	private JLabel titleGPP; // título
+	private JPanel containerTokenLabel;  // Contenedor para el label del token
 	private JLabel tokenLabel; // texto del label del token
+	private JPanel containerTokenField;  // contenedor para el campo del token
 	private JPasswordField tokenField; // campo de texto del token
 	private JPanel containerButton;  // contenedor de botones
 	private JButton loginButton;  // botón de iniciar sesión
@@ -38,8 +45,11 @@ public class VLogin extends JPanel {
 
 		// Creamos los componentes
 		container = new JPanel();
+		containerTitle = new JPanel(new FlowLayout(FlowLayout.CENTER));
 		titleGPP = new JLabel("GitHub Parser Project");
+		containerTokenLabel = new JPanel(new FlowLayout(FlowLayout.LEFT));
 		tokenLabel = new JLabel("Introduce tu token de GitHub:");
+		containerTokenField = new JPanel(new FlowLayout(FlowLayout.LEFT));
 		tokenField = new JPasswordField();
 		containerButton = new JPanel();
 		loginButton = new JButton("Iniciar sesión");
@@ -49,37 +59,52 @@ public class VLogin extends JPanel {
 		titleGPP.setOpaque(true);
 		titleGPP.setBackground(VColor.getGRAY_MENU());
 		titleGPP.setFont(new Font("Dialog", Font.BOLD, 42));
+		containerTitle.setBackground(VColor.getGRAY_MENU());
+		containerTitle.add(titleGPP);
+		
 
 		// Texto del campo del token
 		tokenLabel.setOpaque(true);
 		tokenLabel.setBackground(VColor.getGRAY_MENU());
-		tokenLabel.setBorder(new EmptyBorder(50, 0, 10, 0));
 		tokenLabel.setFont(new Font("Arial", Font.PLAIN, 22));
+		containerTokenLabel.setBackground(VColor.getGRAY_MENU());
+		containerTokenLabel.setBorder(new EmptyBorder(50, 0, 10, 0));
+		containerTokenLabel.add(tokenLabel);
 
 		// Campo de texto del token
 		tokenField.setFont(new Font("Arial", Font.PLAIN, 20));
+		tokenField.setPreferredSize(new Dimension(500, 25));
 		tokenField.setBackground(VColor.getWHITE_MAIN());
 		tokenField.setBorder(BorderFactory.createCompoundBorder(
 				BorderFactory.createLineBorder(VColor.getBLACK_MAIN(), 1), new EmptyBorder(5, 5, 5, 5)));
+		containerTokenField.setBackground(VColor.getGRAY_MENU());
+		containerTokenField.add(tokenField);
 		
 		// Contenedor de botones
-		containerButton.setLayout(new BoxLayout(containerButton, BoxLayout.Y_AXIS));
-		containerButton.add(loginButton, BorderLayout.CENTER);
-		containerButton.add(tokenButton, BorderLayout.CENTER);
-		//containerButton.setBorder(new EmptyBorder(20, 20, 20, 20));
+		//containerButton.setLayout(new BoxLayout(containerButton, BoxLayout.Y_AXIS));
+		containerButton.setLayout(new FlowLayout(FlowLayout.CENTER));
+		containerButton.add(loginButton);
+		containerButton.add(tokenButton);
+		containerButton.setBackground(VColor.getGRAY_MENU());
+		containerButton.setBorder(new EmptyBorder(20, 0, 20, 0));
 
 		// Contenedor general
 		container.setLayout(new BoxLayout(container, BoxLayout.Y_AXIS));
 		container.setBackground(VColor.getGRAY_MENU());
-		container.add(titleGPP);
-		container.add(tokenLabel);
-		container.add(tokenField);
+		container.add(containerTitle);
+		container.add(containerTokenLabel);
+		container.add(containerTokenField);
 		container.add(containerButton);
 		this.add(container, BorderLayout.CENTER);
 
 		// Estilo general
-		this.setBorder(new EmptyBorder(60, 40, 60, 40));
+		this.setBorder(new EmptyBorder(125, 0, 100, 0));
 		this.setBackground(VColor.getGRAY_MENU());
+		
+		// Meter fondo
+		/*Icon myPicture = new ImageIcon("src/main/resources/images/login.jpg");
+		JLabel picLabel = new JLabel(myPicture);
+		add(picLabel);*/
 
 	}
 
